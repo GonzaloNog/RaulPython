3ea30ef6bda49ff7ecccc77f86c01b82932486df
gzeissSRL <63429016+GonzaloNog@users.noreply.github.com>
2023-05-26 14:12:23 -0300

    clase1fix

---
+++ Clase1.py
@@ -0,0 +1,63 @@
+#hello
+print("Hola mundo")
+"""
+print(type("Hola mundo")) #string
+print(type(5)) #int
+print(type(1.5)) #float
+print(type(1 + 5j)) #complex
+print(type(True)) #bool
+print(type(False)) #bool
+print(type([1, 2, 3])) #List
+print(type({'name': 'Gonzalo'})) #Dictionary
+print(type({9.8, 3.14, 2.7})) #Set
+print(type({9.8, 3.14, 2.7})) #tuple
+"""
+
+"""
+comentario
+largo
+python
+
+"""
+
+#Variables 
+
+my_string_variable = 'My string variable'
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_int_variable = 5
+print(my_int_variable + 2)
+
+my_int_to_str_variable = str(my_int_variable) #str()
+
+
+my_int_to_str_variable = 1.5
+intnum = int(my_int_to_str_variable) # int()
+print(intnum)
+print(type(intnum))
+
+
+
+"""
+my_int_variable = 5
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = True
+print(my_bool_variable)
+
+#concatenacion de variables en un print
+print(my_string_variable,my_int_variable,my_bool_variable)
+print('Este es el valor de: ', my_bool_variable)
+
+#funciones del sistema
+print(len(my_string_variable))#len cuenta el largo
+
+#Variables en una sola linea
+name, surname, alias, age = "Gonzalo", "Zeiss", "Gonza", 27
+print(name, surname, age, " y mi alias es ", alias)
+"""

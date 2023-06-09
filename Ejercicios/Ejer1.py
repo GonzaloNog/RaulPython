41041cea6df8071ed0a9d98b20a0b092c88c689e
G1f0 <136001430+G1f0@users.noreply.github.com>
2023-06-09 16:08:54 +0200

    desarrolloEjer01

--- Ejercicios/Ejer1.py
+++ Ejercicios/Ejer1.py
@@ -2,4 +2,9 @@
 Pídale al usuario que ingrese su nombre, su apellido y su edad, estos tienen que estar guardados en una lista, 
 luego muestre todos los datos del usuario en un único print usando format
 '''
+nombre = input("¿Cual es su Nombre?")
+apellido = input(f'Digame su apellido, Sr {nombre.capitalize()}')
+print(f'Me alegro de conocerle, Sr {nombre.capitalize()}{apellido.upper()}')
+edad = int(input (f'Digame su edad, Sr {apellido.upper()}'))
 
+print(f'Estos son sus datos, Sr Nombre: {nombre.capitalize()} Apellido: {apellido.upper()} Edad: {edad}')
--- Ejercicios/Ejer2.py
+++ Ejercicios/Ejer2.py
@@ -4,3 +4,18 @@ luego vamos a preguntarle al usuario si quiere abrir una cuenta bancaria nueva, 
 20.000 euros anuales y si vive en Barcelona en caso de cumplir los requisitos saldrá el mensaje "cuenta completada exitosamente" lo contrario "nos e cumplen
 los requisitos para una nueva cuenta"
 '''
+
+nombre, apellido, edad, sueldo, ciudad = input("Digame su nombre "), input("Digame su apellido "), int(input("Digame su edad ")),int(input("¿Cual es su salario anual? ")),input("¿En que ciudad vive? ")
+print(f'Su nombre es {nombre.capitalize()} {apellido.upper()} su edad es {edad} su salario anual es {sueldo} vive en {ciudad}')
+#my_list = [nombre, apellido, edad, sueldo, ciudad]
+pregunta = str(input (f'¿Quiere abrir una nueva cuenta?, Sr {apellido.upper()}'))
+
+
+
+#if 1 > 0:
+
+
+
+
+
+

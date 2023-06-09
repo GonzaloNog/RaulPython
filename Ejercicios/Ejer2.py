28861f1bc38a87f6391e7940c0ecd4041a130a4f
gzeissSRL <63429016+GonzaloNog@users.noreply.github.com>
2023-06-09 12:01:03 -0300

    ejer2 finish

--- Ejercicios/Ejer2.py
+++ Ejercicios/Ejer2.py
@@ -12,20 +12,26 @@ my_user.append(input(f'Digame su apellido, Sr {my_user[0].capitalize()} '))
 print(f'Me alegro de conocerle, Sr {my_user[0].capitalize()}{my_user[1].upper()} ')
 my_user.append(int(input (f'Digame su edad, Sr {my_user[1].upper()} ')))
 my_user.append(int(input (f'¿cual es su salario anual?, Sr {my_user[1].upper()} ')))
-my_user.append(int(input (f'¿en que ciudad vive?, Sr {my_user[1].upper()} ')))
+my_user.append(input (f'¿en que ciudad vive?, Sr {my_user[1].upper()} '))
 print(f'Estos son sus datos, Sr Nombre: {my_user[0].capitalize()} Apellido: {my_user[1].upper()} Edad: {my_user[2]} Salario: {my_user[3]} Vive en: {my_user[4]}')
 
 
-pregunta = input("Ingrese si o no")
+pregunta = input("Quiere abrir una cuenta de banco nueva Ingrese si o no ")
 
 if pregunta == "si" or pregunta == "SI":
-    print("confirmo")
+    if my_user[3] >= 20000:
+        if my_user[4].capitalize() == "Barcelona":
+            print("Perfecto, su cuenta fue creada")
+        else:
+            print("lamento informarle que tiene qeu residir en Barcelona para poder crear su cuenta")
+    else:
+        print("Lamentablemente usted no cuenta con el ingreso suficiente para abrir una cuenta nueva")
 elif pregunta == 'no' or pregunta == "NO":
-    print("denego")
+    print("Perfecto. que tenga un buen dia")
 else:
     print("tiene que ingresar si o no")
 
-    #
+    
 
 
 

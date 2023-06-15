ff8b0df21fedb343b3e33aafbdec28a1ca5cfed8
gzeissSRL <63429016+GonzaloNog@users.noreply.github.com>
2023-06-15 17:51:59 -0300

    tarea

---
+++ Ejercicios/Ejer4.py
@@ -0,0 +1,7 @@
+'''
+genere un menu con tres opciones.
+ - la primera es registro: ingrese en un set un nuevo documento
+ - nuevo ingreso, salbo que la persona escriba exit, ingrese todos los documentos que quiera en un nuevo set(si algun documento no esta registrado marque el error al usuario), 
+ al finalisar muestre en pantalla los trabajadores que faltaron a la empresa
+ - cerrar programa
+'''

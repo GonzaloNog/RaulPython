1f0213c9a23f74806cb25d98feaf6bae8fc5f9c1
gzeissSRL <63429016+GonzaloNog@users.noreply.github.com>
2023-06-26 10:42:03 -0300

    ejercicios

---
+++ Clase7.py
@@ -0,0 +1,24 @@
+### Funciones ###
+
+
+def my_funtion():
+    print('Esto es una funcion')
+
+my_funtion()
+my_funtion()
+
+def sum_two_values(first_value,second_value):
+    print(first_value + second_value)
+
+def changeNum(num):
+    num = 20
+
+numero = 10
+changeNum(numero)
+print(numero)
+
+def changeNumRet(num):
+    return num + 20
+
+numero = changeNumRet(10)
+print(numero)
